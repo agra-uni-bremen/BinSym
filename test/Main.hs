73b9bd3421e5fad70df38f9b0205c0b4575e5b22
Sören Tempel <tempel@uni-bremen.de>
2023-05-16 18:14:37 +0200

    Test writing register file too

--- test/Main.hs
+++ test/Main.hs
@@ -21,15 +21,31 @@ tests = testGroup "Tests"
 readZeroReg :: Z3.Z3 (Maybe [Integer])
 readZeroReg = do
   regFile <- mkRegFile
-  x1 <- mkSymWord32 0
+  x1 <- mkSymWord32 1
   value <- readRegister regFile x1
 
   fmap snd $ Z3.withModel $ \m ->
     catMaybes <$> mapM (Z3.evalBv False m) [value]
+
+writeRegFile :: Z3.Z3 (Maybe [Integer])
+writeRegFile = do
+  regFile <- mkRegFile
+  x1 <- mkSymWord32 1
+  value <- readRegister regFile x1
+
+  newValue <- mkSymWord32 42
+  newRegFile <- writeRegister regFile x1 newValue
+  readValue <- readRegister newRegFile x1
+
+  fmap snd $ Z3.withModel $ \m ->
+    catMaybes <$> mapM (Z3.evalBv False m) [readValue]
 
 registerTests :: TestTree
 registerTests = testGroup "RegisterFile tests"
   [ testCase "Read default register value" $ do
       (Just v) <- Z3.evalZ3 readZeroReg
       assertEqual "must be zero" v [0]
+  , testCase "Write and read register file" $ do
+      (Just v) <- Z3.evalZ3 writeRegFile
+      assertEqual "must be 42" v [42]
   ]
