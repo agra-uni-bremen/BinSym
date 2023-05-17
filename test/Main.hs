3f822fafdfde94e7439f367787dbb79cdaeb0159
Sören Tempel <tempel@uni-bremen.de>
2023-05-17 17:20:31 +0200

    test: Refactor

--- test/Main.hs
+++ test/Main.hs
@@ -3,8 +3,8 @@ import Test.Tasty.HUnit
 
 import Register
 import Util
+import TestUtils
 
-import Data.Maybe (catMaybes)
 import qualified Z3.Monad as Z3
 
 main :: IO ()
@@ -18,48 +18,33 @@ tests = testGroup "Tests"
 
 ------------------------------------------------------------------------
 
-readZeroReg :: Z3.Z3 (Maybe [Integer])
-readZeroReg = do
-  regFile <- mkRegFile
-  x1 <- mkSymWord32 1
-  value <- readRegister regFile x1
-
-  fmap snd $ Z3.withModel $ \m ->
-    catMaybes <$> mapM (Z3.evalBv False m) [value]
-
-writeRegFile :: Z3.Z3 (Maybe [Integer])
-writeRegFile = do
-  regFile <- mkRegFile
-  x1 <- mkSymWord32 1
-
-  newValue <- mkSymWord32 42
-  writeRegister regFile x1 newValue
-  readValue <- readRegister regFile x1
-
-  fmap snd $ Z3.withModel $ \m ->
-    catMaybes <$> mapM (Z3.evalBv False m) [readValue]
-
-writeZeroRegister :: Z3.Z3 (Maybe [Integer])
-writeZeroRegister = do
-  regFile <- mkRegFile
-  x0 <- mkSymWord32 0
-
-  newValue <- mkSymWord32 42
-  writeRegister regFile x0 newValue
-  readValue <- readRegister regFile x0
-
-  fmap snd $ Z3.withModel $ \m ->
-    catMaybes <$> mapM (Z3.evalBv False m) [readValue]
-
 registerTests :: TestTree
 registerTests = testGroup "RegisterFile tests"
   [ testCase "Read default register value" $ do
-      (Just v) <- Z3.evalZ3 readZeroReg
-      assertEqual "must be zero" [0] v
+      (Just v) <- Z3.evalZ3 $ do
+        regFile <- mkRegFile
+        x1 <- mkSymWord32 1
+        readRegister regFile x1 >>= getInt
+
+      assertEqual "default register value is zero" 0 v
   , testCase "Write and read register file" $ do
-      (Just v) <- Z3.evalZ3 writeRegFile
-      assertEqual "must be 42" [42] v
+      (Just v) <- Z3.evalZ3 $ do
+        regFile <- mkRegFile
+        x1 <- mkSymWord32 1
+
+        newValue <- mkSymWord32 42
+        writeRegister regFile x1 newValue
+        readRegister regFile x1 >>= getInt
+
+      assertEqual "readRegister must return last written value" 42 v
   , testCase "Attempt to write the zero register" $ do
-      (Just v) <- Z3.evalZ3 writeZeroRegister
-      assertEqual "must be 0" [0] v
+      (Just v) <- Z3.evalZ3 $ do
+        regFile <- mkRegFile
+        x0 <- mkSymWord32 0
+
+        newValue <- mkSymWord32 42
+        writeRegister regFile x0 newValue
+        readRegister regFile x0 >>= getInt
+
+      assertEqual "writes to zero register are ignored" 0 v
   ]
---
+++ test/TestUtils.hs
@@ -0,0 +1,15 @@
+module TestUtils where
+
+import qualified Z3.Monad as Z3
+import Data.Maybe (catMaybes)
+import Util
+
+getInts :: [Z3.AST] -> Z3.Z3 (Maybe [Integer])
+getInts values =
+  fmap snd $ Z3.withModel $ \m ->
+    catMaybes <$> mapM (Z3.evalBv False m) values
+
+getInt :: Z3.AST -> Z3.Z3 (Maybe Integer)
+getInt v = do
+  ints <- getInts [v]
+  pure $ head <$> ints
