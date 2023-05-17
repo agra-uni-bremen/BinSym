cb9e5e2e0bf4633f1e69ff3a2b9b07dbe8806f22
Sören Tempel <tempel@uni-bremen.de>
2023-05-17 15:12:38 +0200

    Interpreter: Implement remaining binary operations

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -33,11 +33,20 @@ binOp e1 e2 op = do
   op bv1 bv2
 
 evalE :: Z3.MonadZ3 z3 => E.Expr (Z3.AST) -> z3 Z3.AST
-evalE (E.FromImm e) = pure e
+evalE (E.FromImm e)  = pure e
 evalE (E.FromUInt v) = mkSymWord32 v
-evalE (E.Add e1 e2) = binOp e1 e2 Z3.mkBvadd
-evalE (E.Sub e1 e2) = binOp e1 e2 Z3.mkBvsub
-evalE (E.Eq e1 e2) = binOp e1 e2 Z3.mkEq >>= fromBool
+evalE (E.Add e1 e2)  = binOp e1 e2 Z3.mkBvadd
+evalE (E.Sub e1 e2)  = binOp e1 e2 Z3.mkBvsub
+evalE (E.Eq e1 e2)   = binOp e1 e2 Z3.mkEq     >>= fromBool
+evalE (E.Slt e1 e2)  = binOp e1 e2 Z3.mkBvslt  >>= fromBool
+evalE (E.Sge e1 e2)  = binOp e1 e2 Z3.mkBvsge  >>= fromBool
+evalE (E.Uge e1 e2)  = binOp e1 e2 Z3.mkBvuge  >>= fromBool
+evalE (E.And e1 e2)  = binOp e1 e2 Z3.mkBvand  >>= fromBool
+evalE (E.Or e1 e2)   = binOp e1 e2 Z3.mkBvor   >>= fromBool
+evalE (E.Xor e1 e2)  = binOp e1 e2 Z3.mkBvxor  >>= fromBool
+evalE (E.LShl e1 e2) = binOp e1 e2 Z3.mkBvshl
+evalE (E.LShr e1 e2) = binOp e1 e2 Z3.mkBvlshr
+evalE (E.AShr e1 e2) = binOp e1 e2 Z3.mkBvashr
 evalE _ = error "expression language operation not implemented"
 
 ------------------------------------------------------------------------
