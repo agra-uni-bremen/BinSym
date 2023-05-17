43994cfaa7489662740ffffc6db3cdce83d8c906
Sören Tempel <tempel@uni-bremen.de>
2023-05-17 20:48:54 +0200

    Interpreter: Start working on free monad interpreter

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -3,19 +3,22 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeFamilies #-}
 module Interpreter where
 
 import Util
+import qualified Register as REG
 
 import Control.Monad.Freer
 import Data.Word (Word32)
 import Data.Array.IO (IOArray)
 import LibRISCV (ByteAddrsMem(..), Address)
 import LibRISCV.Spec.Operations (Operations(..))
+import Control.Monad.IO.Class (liftIO)
 
 import qualified Z3.Monad as Z3
 import qualified LibRISCV.Spec.Expr as E
-import qualified LibRISCV.Machine.Register as REG
 import qualified LibRISCV.Machine.Memory as MEM
 
 -- Map a binary operation in the LibRISCV expression language to a Z3 operation.
@@ -38,20 +41,24 @@ evalE _ = error "expression language operation not implemented"
 
 ------------------------------------------------------------------------
 
-type ArchState = (REG.RegisterFile IOArray Z3.AST, MEM.Memory IOArray Z3.AST)
+type ArchState = (REG.RegisterFile, MEM.Memory IOArray Z3.AST)
 
 -- instance ByteAddrsMem ArchState where
 --   storeByteString (_, mem) = MEM.storeByteString mem
 
-mkArchState :: Address -> Word32 -> Z3.AST -> IO ArchState
-mkArchState memStart memSize zeroVal = do
-    reg <- REG.mkRegFile zeroVal
-    mem <- MEM.mkMemory memStart memSize
+mkArchState :: Z3.MonadZ3 z3 => Address -> Word32 -> z3 ArchState
+mkArchState memStart memSize = do
+    reg <- REG.mkRegFile
+    mem <- liftIO $ MEM.mkMemory memStart memSize
     pure (reg, mem)
 
 ------------------------------------------------------------------------
 
 type SymEnv m = (E.Expr Z3.AST -> m Z3.AST, ArchState)
 
-symBehavior :: Z3.MonadZ3 z3 => SymEnv z3 -> Operations (Z3.AST) ~> IO
-symBehavior _ _ = undefined
+symBehavior :: Z3.MonadZ3 z3 => SymEnv z3 -> Operations (Z3.AST) ~> z3
+symBehavior (eval, (regFile, _mem)) = \case
+  ReadRegister idx -> REG.readRegister regFile idx
+  WriteRegister idx val -> eval val >>= REG.writeRegister regFile idx
+  WritePC newPC -> eval newPC >>= REG.writePC regFile
+  ReadPC -> REG.readPC regFile
