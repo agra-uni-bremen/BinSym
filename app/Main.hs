0b5fb1a7508b245617a194a4a70a583a0414c1b1
Sören Tempel <tempel@uni-bremen.de>
2023-06-12 13:22:57 +0200

    Allow for optional deterministic random number generation
    To ease debugging

--- app/Main.hs
+++ app/Main.hs
@@ -22,7 +22,26 @@ import SymEx.Interpreter
 import SymEx.Memory (storeByteString)
 import qualified SymEx.Store as S
 import SymEx.Tracer
+import System.Random (initStdGen, mkStdGen, setStdGen)
 import qualified Z3.Monad as Z3
+
+data SymbolicArgs = SymbolicArgs
+  { randSeed :: Maybe Int,
+    base :: BasicArgs
+  }
+
+{- ORMOLU_DISABLE -}
+symbolicArgs :: Parser SymbolicArgs
+symbolicArgs =
+  SymbolicArgs
+    <$> optional
+      (option auto
+        ( long "random-seed"
+        <> help "Seed for the random number generator"))
+    <*> basicArgs
+{- ORMOLU_ENABLE -}
+
+------------------------------------------------------------------------
 
 runPath :: forall z3. (Z3.MonadZ3 z3) => BasicArgs -> S.Store -> z3 ExecTrace
 runPath (BasicArgs memAddr memSize verbose putReg fp) store = do
@@ -70,15 +89,22 @@ runAll numPaths args store tree = do
       liftIO $ putStrLn ("\nNext assignment:\n" ++ show newStore)
       runAll (numPaths + 1) args newStore (Just nextTree)
 
-main' :: forall z3. (Z3.MonadZ3 z3) => BasicArgs -> z3 ()
-main' args = runAll 1 args S.empty Nothing
+main' :: forall z3. (Z3.MonadZ3 z3) => SymbolicArgs -> z3 ()
+main' (SymbolicArgs seed args) = do
+  -- Optional deterministic random number generation for debugging.
+  stdgen <- case seed of
+    Just x -> pure $ mkStdGen x
+    Nothing -> initStdGen
+  liftIO $ setStdGen stdgen
+
+  runAll 1 args S.empty Nothing
 
 main :: IO ()
 main = (Z3.evalZ3 . main') =<< execParser opts
   where
     opts =
       info
-        (basicArgs <**> helper)
+        (symbolicArgs <**> helper)
         ( fullDesc
             <> progDesc "Symbolic execution of RISC-V machine code"
         )
